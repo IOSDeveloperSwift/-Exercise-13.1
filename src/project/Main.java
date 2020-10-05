1d1ec44571e2f49cb75fc7f574f158bf787acb2c
Dima <doloshkan96@mail.ru>
2020-10-05 18:00:58 +0300

    ex.13.1

---
+++ src/project/Main.java
@@ -0,0 +1,43 @@
+package project;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> arrList = new ArrayList();
+
+        for (int j = 1; j <= 10; j++) {
+            arrList.add(j);
+        }
+
+//        while loop
+//        int i = 0;
+//        while (i < arrList.size()) {
+//            System.out.println(arrList.get(i));
+//            i++;
+//        }
+
+//        Iterator
+//        Iterator iterator = arrList.iterator();
+//        while (iterator.hasNext()){
+//            System.out.println(iterator.next());
+//        }
+
+//        enhanced for loop
+//        for (int i : arrList){
+//            System.out.println(i);
+//        }
+
+//        for loop
+//        for (int i = 0; i < arrList.size(); i++) {
+//            System.out.println(arrList.get(i));
+//        }
+
+    }
+}
+
